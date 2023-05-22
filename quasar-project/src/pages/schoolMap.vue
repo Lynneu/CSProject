f98ba6560a519eb82f491ec2fe95228a08213001
Lynneu <Lynneu@163.com>
2023-05-22 22:31:27 +0800

    重构

--- quasar-project/src/pages/schoolMap.vue
+++ quasar-project/src/pages/schoolMap.vue
@@ -3,10 +3,10 @@
     <div>
       <div class="q-pa-md">
         <keep-alive>
-            <component :is='current'></component>
-          </keep-alive>
+          <component :is="current"></component>
+        </keep-alive>
         <q-page-sticky position="top-left" :offset="[18, 18]">
-        <q-btn-toggle
+          <q-btn-toggle
             v-model="current"
             spread
             no-caps
@@ -14,39 +14,37 @@
             color="white"
             text-color="black"
             :options="[
-              {label: '校内', value: 'schoolmap'},
-              {label: '校外', value: 'allmap'}
+              { label: '校内', value: 'schoolmap' },
+              { label: '校外', value: 'allmap' },
             ]"
           />
         </q-page-sticky>
-
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import schoolmap from '../components/SchoolMapComponent.vue'
-import allmap from '../components/AllMapComponent.vue'
+import schoolmap from '../components/SchoolMapComponent.vue';
+import allmap from '../components/AllMapComponent.vue';
 
 export default {
   components: { schoolmap, allmap },
   data() {
     return {
-      current: 'schoolmap'
-    }
-  }
-}
-
+      current: 'schoolmap',
+    };
+  },
+};
 </script>
 
 <style>
 #container {
   width: 100%;
- /* height: 546px;*/
- height: 100%;
- padding: 0px;
-        margin: 0px;
+  /* height: 546px;*/
+  height: 100%;
+  padding: 0px;
+  margin: 0px;
   /* border: 1px solid rgb(12, 88, 107);*/
 }
 </style>
